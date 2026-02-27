39c6f09fc758a9be3e3a928596291be238c8352d
nk6785 <navadeepkintFBUKIRGi@email.com>
2026-02-27 16:20:13 +0530

    [pracheet] commited usecase

---
+++ App/src/Palindrome.java
@@ -0,0 +1,8 @@
+public class Palindrome {
+    static void main() {
+        System.out.println("welcome to polindrome checkerApp");
+        System.out.println("version 1.0");
+        System.out.println("system initialised");
+
+    }
+}
